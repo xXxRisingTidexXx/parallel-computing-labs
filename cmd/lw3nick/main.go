99e62e35ecf1a9f0a71cd8cd3ef4594cfe806176
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-02-21 21:51:49 +0200

    Added semaphore

---
+++ cmd/lw3nick/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/xXxRisingTidexXx/parallel-computing-labs/internal/sem"
+	"sync"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	semaphore, group := sem.NewSemaphore(10), &sync.WaitGroup{}
+	group.Add(60)
+	for i := 0; i < 60; i++ {
+		go work(i, semaphore, group)
+	}
+	group.Wait()
+	fmt.Printf("elapsed time %s\n", time.Since(now))
+}
+
+func work(i int, semaphore *sem.Semaphore, group *sync.WaitGroup) {
+	semaphore.Lock()
+	time.Sleep(time.Second)
+	fmt.Printf("worker-%d\n", i)
+	semaphore.Unlock()
+	group.Done()
+}
---
+++ internal/sem/semaphore.go
@@ -0,0 +1,24 @@
+package sem
+
+import (
+	"fmt"
+)
+
+func NewSemaphore(size int) *Semaphore {
+	if size < 1 {
+		panic(fmt.Sprintf("sem: semaphore got invalid size, %d < 1", size))
+	}
+	return &Semaphore{make(chan struct{}, size)}
+}
+
+type Semaphore struct {
+	hits chan struct{}
+}
+
+func (s *Semaphore) Lock() {
+	s.hits <- struct{}{}
+}
+
+func (s *Semaphore) Unlock() {
+	<-s.hits
+}
