bdb0e6b818db6c03c96df7f50db1090daf0445dc
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-05-14 00:10:35 +0300

    Added docker stuff

--- cmd/lw10/main.go
+++ cmd/lw10/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	_ "github.com/lib/pq"
+	log "github.com/sirupsen/logrus"
+	"os"
 )
 
 func main() {
-
+	db, err := sql.Open("postgres", os.Getenv("SEARCH_DSN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		_ = db.Close()
+		log.Fatal(err)
+	}
+	if err := db.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
