07929a53672a0f40829f34617e3d4ba81a0ab0ed
xXxRisingTidexXx <danylopetrik@gmail.com>
2021-05-15 03:10:34 +0300

    Added document retrieval

--- cmd/lw10/main.go
+++ cmd/lw10/main.go
@@ -5,7 +5,6 @@ import (
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
 	"os"
-	"os/signal"
 )
 
 func main() {
@@ -17,10 +16,37 @@ func main() {
 		_ = db.Close()
 		log.Fatal(err)
 	}
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt, os.Kill)
-	<-signals
+	documents, err := readDocuments(db)
+	if  err != nil {
+		_ = db.Close()
+		log.Fatal(err)
+	}
 	if err := db.Close(); err != nil {
 		log.Fatal(err)
 	}
+	log.Infof("Read %d documents", len(documents))
+}
+
+func readDocuments(db *sql.DB, terms ...string) ([]string, error) {
+	rows, err := db.Query(`select text from documents`)
+	if err != nil {
+		return nil, err
+	}
+	documents := make([]string, 0)
+	for rows.Next() {
+		var document string
+		if err := rows.Scan(&document); err != nil {
+			_ = rows.Close()
+			return nil, err
+		}
+		documents = append(documents, document)
+	}
+	if err := rows.Err(); err != nil {
+		_ = rows.Close()
+		return nil, err
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	return documents, nil
 }
